2adee3d44996d9f38f3de561f729cd1b012c4907
macPry <maciej.przybyl@elpassion.pl>
2017-06-09 17:18:46 +0200

    Change shape

--- app/src/main/java/pl/elpassion/animations/MainActivity.kt
+++ app/src/main/java/pl/elpassion/animations/MainActivity.kt
@@ -4,8 +4,12 @@ import android.animation.AnimatorInflater
 import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +21,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setButtons() {
-        oneButton.setOnClickListener{ changeColor(it) }
+        oneButton.setOnClickListener { changeColor(it) }
+        twoButton.setOnClickListener { changeShape(two) }
     }
 
     fun changeColor(view: View) {
@@ -25,5 +30,40 @@ class MainActivity : AppCompatActivity() {
         animator.setEvaluator(ArgbEvaluator())
         animator.target = view
         animator.start()
+    }
+
+    fun changeShape(view: ImageView) {
+        val squareOutAnim = AnimationUtils.loadAnimation(this, android.R.anim.fade_out)
+        squareOutAnim.duration = 2000
+        val circleInAnim = AnimationUtils.loadAnimation(this, android.R.anim.fade_in)
+        circleInAnim.duration = 2000
+        val circleOutAnim = AnimationUtils.loadAnimation(this, android.R.anim.fade_out)
+        circleOutAnim.duration = 2000
+        val squareInAnim = AnimationUtils.loadAnimation(this, android.R.anim.fade_in)
+        squareInAnim.duration = 2000
+        squareOutAnim.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+            override fun onAnimationRepeat(animation: Animation?) {}
+            override fun onAnimationEnd(animation: Animation?) {
+                view.setImageDrawable(ContextCompat.getDrawable(this@MainActivity, R.drawable.circle))
+                view.startAnimation(circleInAnim)
+            }
+        })
+        circleInAnim.setAnimationListener(object :Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+            override fun onAnimationRepeat(animation: Animation?) {}
+            override fun onAnimationEnd(animation: Animation?) {
+                view.startAnimation(circleOutAnim)
+            }
+        })
+        circleOutAnim.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+            override fun onAnimationRepeat(animation: Animation?) {}
+            override fun onAnimationEnd(animation: Animation?) {
+                view.setImageDrawable(ContextCompat.getDrawable(this@MainActivity, R.drawable.square))
+                view.startAnimation(squareInAnim)
+            }
+        })
+        view.startAnimation(squareOutAnim)
     }
 }
